e494ef06663102712f185ba829980589ba270840
LuisMiranda <luis-miranda@lasallistas.org.mx>
2020-03-31 20:21:30 -0600

    Update ZooChapu.py

--- ZooChapu.py
+++ ZooChapu.py
@@ -1,1 +1,8 @@
 #Queremos una lista donde se puedan ingresar animales
+list = []
+class Animal:
+	def __init__(self):
+		#Aqui irán los inputs
+	def NewType(self):
+		#Se registrará los nuevos tipos
+	
