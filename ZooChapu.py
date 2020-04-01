8d12f2e5269a77ed048450af1919952d7c5f4f7b
LuisMiranda <luis-miranda@lasallistas.org.mx>
2020-03-31 21:18:56 -0600

    Update ZooChapu.py
    Cambios para pruebas tanto funcion NewType como while

--- ZooChapu.py
+++ ZooChapu.py
@@ -6,33 +6,32 @@ class Animal:
 		#Aqui irán los inputs
 		cont =int(input("¿Cuántos animales entran?: "))
 		for x in range (0,cont):
-			
 			self.name=input("¿Cuál es el nombre del animal?: ")
 			lista.append(self.name)
-			
 			self.type = input("¿Qué tipo de animal es?: ")
 			list_type.append(self.type)	
 			print ("El animal "+self.name+" ha ingresado y es del tipo: "+self.type)
-		
 		print("La lista actual de animales es: " + lista[:])
-		bool validar = true; 
+		validar = true
 		while validar:
 			id = int(input("Para cambiar el tipo de animal, escoge a un animal de la lista: "))
-			if(id==0){
-			print ("La lista empieza del 1 a n")
-			print ("")
-			}
-			else
-			{
-			id--
-			elegido = lista[id]
-			elegido_t= list_type[id]
-			print("El nombre del animal elegido es: "+ dato + " y es del tipo: " + elegido_t)
-			self.NewType();
-			}
-			pass
+			if(id==0):
+				print ("La lista empieza del 1 a n")
+				print ("")
+			else:
+				id--
+				elegido = lista[id]
+				elegido_t= list_type[id]
+				print("El nombre del animal elegido es: "+ dato + " y es del tipo: " + elegido_t)
+				self.NewType(id)
+				validar=false
+		pass
 
-	def NewType(self):
+	def NewType(id):
 		#Se registrará los nuevos tipos
-		self.type = input ("¿Cuál es el nuevo tipo de animal?")
+		self.type = input ("¿Cuál es el nuevo tipo de animal?: ")
+		list_type[id] = self.type
+		elegido = lista[id]
+		elegido_t= list_type[id]
+		print ("El nuevo tipo es " + elegido_t + " del animal " + "elegido")
 new_animal = Animal()
